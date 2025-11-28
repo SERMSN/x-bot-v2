215dc026d7e8976b5fe78397f40802d67075dc60
SerMSN <sermsn@gmail.com>
2025-11-28 17:09:04 +0700

    Init

---
+++ src/app/Providers/Filament/MainPanelProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages\Dashboard;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets\AccountWidget;
+use Filament\Widgets\FilamentInfoWidget;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+use Filament\Navigation\NavigationItem;
+
+class MainPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->id('main')
+            ->path('/')
+            ->homeUrl('/')
+            ->topNavigation()
+            ->brandName('X-Bot')
+            ->favicon(asset('favicon.png'))
+            ->colors([
+                'primary' => '#3B82F6',
+            ])
+            ->discoverResources(in: app_path('Filament/Main/Resources'), for: 'App\Filament\Main\Resources')
+            ->discoverPages(in: app_path('Filament/Main/Pages'), for: 'App\Filament\Main\Pages')
+            ->pages([
+                Dashboard::class,
+            ])
+            //->discoverWidgets(in: app_path('Filament/Main/Widgets'), for: 'App\Filament\Main\Widgets')
+          /*  ->widgets([
+                AccountWidget::class,
+                FilamentInfoWidget::class,
+            ])*/
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ]);
+          /*  ->authMiddleware([
+                Authenticate::class,
+            ]);*/
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+/*
+use App\Filament\Main\Resources\HomePageResource;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+/*
+// Главная страница через Filament
+Route::get('/', function () {
+    return app(\Filament\Http\Responses\Auth\Contracts\LoginResponse::class);
+})->name('home');
+*/
