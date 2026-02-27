d5ae349e776372e10ec3c7c55a768fce3342294f
WRCS <sermsn@gmail.com>
2026-02-28 00:22:12 +0700

    Добавлены на главную публичную часть два виджет количество ботов и чатов

--- src/app/Providers/Filament/MainPanelProvider.php
+++ src/app/Providers/Filament/MainPanelProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Main\Widgets\BotsCountWidget;
+use App\Filament\Main\Widgets\ChatsCountWidget;
+use App\Filament\Main\Pages\Dashboard;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
@@ -19,33 +21,35 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-use Filament\Navigation\NavigationItem;
-
 class MainPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->id('main')
-            ->path('/')
+            ->id("main")
+            ->path("/")
             //->homeUrl('/')
-            ->homeUrl(env('APP_URL'))
+            ->homeUrl(env("APP_URL"))
             ->topNavigation()
-            ->brandName('X-Bot')
-            ->favicon(asset('favicon.png'))
+            ->brandName("X-Bot")
+            ->favicon(asset("favicon.png"))
             ->colors([
-                'primary' => Color::Indigo,
+                "primary" => Color::Indigo,
             ])
-            ->discoverResources(in: app_path('Filament/Main/Resources'), for: 'App\Filament\Main\Resources')
-            ->discoverPages(in: app_path('Filament/Main/Pages'), for: 'App\Filament\Main\Pages')
-            ->pages([
-                Dashboard::class,
-            ])
-            //->discoverWidgets(in: app_path('Filament/Main/Widgets'), for: 'App\Filament\Main\Widgets')
-          /*  ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
-            ])*/
+            ->discoverResources(
+                in: app_path("Filament/Main/Resources"),
+                for: "App\Filament\Main\Resources",
+            )
+            ->discoverPages(
+                in: app_path("Filament/Main/Pages"),
+                for: "App\Filament\Main\Pages",
+            )
+            ->pages([Dashboard::class])
+            ->discoverWidgets(
+                in: app_path("Filament/Main/Widgets"),
+                for: "App\Filament\Main\Widgets",
+            )
+            ->widgets([BotsCountWidget::class, ChatsCountWidget::class])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -57,7 +61,7 @@ class MainPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ]);
-          /*  ->authMiddleware([
+        /*  ->authMiddleware([
                 Authenticate::class,
             ]);*/
     }
